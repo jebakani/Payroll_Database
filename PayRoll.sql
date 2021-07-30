2ca1d18d36b934f755c2404e6844182ed6f3a54b
Jebakani <vjishwarya@gmail.com>
2021-07-30 20:59:08 +0530

    [JEBAKANI] Retriving all the data from the table

--- PayRoll.sql
+++ PayRoll.sql
@@ -11,4 +11,10 @@ startDate date
 )
 --UC3-Inserting the data into the table
 INSERT INTO PayRollTable VALUES('JEBA',10000,'2021-07-30')
+INSERT INTO PayRollTable VALUES('aabc',10000,'2020-07-17')
+INSERT INTO PayRollTable VALUES('xyz',10000,'2021-07-30')
+INSERT INTO PayRollTable VALUES('aaa',15293,'2021-07-30')
+
+--UC4-Retriving the data from the table
+SELECT * FROM PayRollTable
 
