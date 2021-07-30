ff04ff1427e19f52bf8e4b8522d3635e1a5c97a4
Jebakani <vjishwarya@gmail.com>
2021-07-30 20:41:14 +0530

    [JEBAKANI] creating the table with atribute id,name,salarya and start date

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,2 +1,11 @@
 --UC1-Creating database for the employee payroll  
 create database Employee_Payroll_Database
+
+--UC2-Creating table for employee payroll with attributes columns id,name, salary and start date
+create table PayRollTable
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary float,
+startDate date
+)
